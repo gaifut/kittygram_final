cc56a9c85a84dc8ace47eba497753cbd63ae1c08
Artur Gayfutdinov <gaifut@gmail.com>
2024-02-02 15:33:20 +0300

    changed bool debug

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', default=False).lower()=='true'
+DEBUG = os.getenv('DEBUG', default=False)=='True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(',')
 
