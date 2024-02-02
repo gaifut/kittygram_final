fc8f2f45f278642e9d2cb09965170878e4e6550e
Artur Gayfutdinov <gaifut@gmail.com>
2024-02-02 12:05:51 +0300

    intermediary changes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,12 +1,13 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key as grsk
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', default=grsk())
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
